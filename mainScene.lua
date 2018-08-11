4883cdf7f041b4df1632b751d2867a270e671454
ITJEONG <derbana1027@gmail.com>
2018-08-11 22:35:24 +0900

    ADD : init UI

--- mainScene.lua
+++ mainScene.lua
@@ -33,6 +33,8 @@ function scene:show( event )
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
 
+        initUI()
+
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
 
